af2745d99b829bf924368bd8961f1a19af893e28
bentwhiskers <abbott1414@gmail.com>
2023-04-17 21:46:35 -0400

    part of lvl two function working in JS, situation appears on screen but buttons do not have text in them yet

--- Assets/script.js
+++ Assets/script.js
@@ -1,43 +1,43 @@
 
-//api request giphy
+// //api request giphy
 
-//level 3
+// //level 3
 
-//renderSituation 
+// //renderSituation 
 
 
-// var apiKey = "X13IhKtYw4lctizGutpMfHbcFQ0pTaaY"
-// function showGIF() {
-//   fetch(``)
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data)
+// // var apiKey = "X13IhKtYw4lctizGutpMfHbcFQ0pTaaY"
+// // function showGIF() {
+// //   fetch(``)
+// //   .then(response => response.json())
+// //   .then(data => {
+// //     console.log(data)
 
-var requestUrl = "https://api.giphy.com/v1/gifs/search?q=stressed&api_key=X13IhKtYw4lctizGutpMfHbcFQ0pTaaY&limit=1"
+// var requestUrl = "https://api.giphy.com/v1/gifs/search?q=stressed&api_key=X13IhKtYw4lctizGutpMfHbcFQ0pTaaY&limit=1"
 
-var resultImg = document.getElementById('resultImg');
+// var resultImg = document.getElementById('resultImg');
 
-function getApi(requestUrl) {
-  fetch(requestUrl)
-    .then(function (response) {
-      console.log(response);
-      if (response.status === 200) {
-        return response.json();
-      } else {
-      console.log(response.statusText)
-      }
-       })
-       .then(function (data){
-        console.log(data)
-        document.getElementById('resultImg').innerHTML = "<img src='" + data.data[0].images.original.url +"' />";
-      })
-    }
+// function getApi(requestUrl) {
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       console.log(response);
+//       if (response.status === 200) {
+//         return response.json();
+//       } else {
+//       console.log(response.statusText)
+//       }
+//        })
+//        .then(function (data){
+//         console.log(data)
+//         document.getElementById('resultImg').innerHTML = "<img src='" + data.data[0].images.original.url +"' />";
+//       })
+//     }
 
 
     
       
 
-getApi(requestUrl);
+// getApi(requestUrl);
 
 
 
@@ -338,72 +338,72 @@ getApi(requestUrl);
 
 
 
-//user enters their name
-//user clicks submit name button
-//when user clicks submit name, their name is stored in local storage
-//when user clicks submit name, start game button appears
-//when user clicks start game, it takes them to the next question and hides the start game screen
+// //user enters their name
+// //user clicks submit name button
+// //when user clicks submit name, their name is stored in local storage
+// //when user clicks submit name, start game button appears
+// //when user clicks start game, it takes them to the next question and hides the start game screen
 
-var submitBtnEl = document.getElementById('submit-name-btn');
-var welcomeEl = document.getElementById('welcome-card');
-var startBtnEl = document.getElementById('start-game-btn');
+// var submitBtnEl = document.getElementById('submit-name-btn');
+// var welcomeEl = document.getElementById('welcome-card');
+// var startBtnEl = document.getElementById('start-game-btn');
 
-var username = localStorage.getItem("username");
+// var username = localStorage.getItem("username");
 
-function submitName() {
-    submitBtnEl.addEventListener('click', function(event) {
-        event.preventDefault();
-        var userInput = document.getElementById('inputVal').value;
-        localStorage.setItem("username", userInput)
-        let element = document.getElementById("start-game-btn");
-            element.removeAttribute("hidden"); 
-    })
-}
+// function submitName() {
+//     submitBtnEl.addEventListener('click', function(event) {
+//         event.preventDefault();
+//         var userInput = document.getElementById('inputVal').value;
+//         localStorage.setItem("username", userInput)
+//         let element = document.getElementById("start-game-btn");
+//             element.removeAttribute("hidden"); 
+//     })
+// }
 
-submitName()
+// submitName()
 
-//function toggle () {
-//    var 
-//}
+// //function toggle () {
+// //    var 
+// //}
 
 
-var levelOneSituations = [
-    {
-        situation: "You’re interested in attending a coding bootcamp! You’ve selected Butler University’s full stack web dev program, but it looks like a big commitment. The website has a submission form to inquire more and get in touch with an admissions counselor, so you enter your info and get an email back the next day.",
-        cOne: "Open the email right away, and email back. You’re excited to get the process started!",
-        cTwo: "Wait a week to open the email and another week to reply. You’ve got time!"
-    }, {
-        situation: "You’ve gotten in touch with the advisor and they have given you a bunch of info about the program. It’s important that you respond in a timely manner, since there are deadlines.",
-        cOne: "Make the phone calls, send the emails, and set up the accounts with plenty of time before the program begins.",
-        cTwo: "Procrastinate until the last few days before the program begins to tie up all the loose ends. You make it into the program, with an hour to spare.",
-    }, {
-        situation: "You’re assigned Pre-Work! You're tasked with building a website from scratch.",
-        cOne: "Spend any time remaining before the first day of class available to try to complete the Pre-Work.",
-        cTwo: "Don’t work on it, you don’t know any of the material and you're going to learn it in class anyway."
-    }
-];
+// var levelOneSituations = [
+//     {
+//         situation: "You’re interested in attending a coding bootcamp! You’ve selected Butler University’s full stack web dev program, but it looks like a big commitment. The website has a submission form to inquire more and get in touch with an admissions counselor, so you enter your info and get an email back the next day.",
+//         cOne: "Open the email right away, and email back. You’re excited to get the process started!",
+//         cTwo: "Wait a week to open the email and another week to reply. You’ve got time!"
+//     }, {
+//         situation: "You’ve gotten in touch with the advisor and they have given you a bunch of info about the program. It’s important that you respond in a timely manner, since there are deadlines.",
+//         cOne: "Make the phone calls, send the emails, and set up the accounts with plenty of time before the program begins.",
+//         cTwo: "Procrastinate until the last few days before the program begins to tie up all the loose ends. You make it into the program, with an hour to spare.",
+//     }, {
+//         situation: "You’re assigned Pre-Work! You're tasked with building a website from scratch.",
+//         cOne: "Spend any time remaining before the first day of class available to try to complete the Pre-Work.",
+//         cTwo: "Don’t work on it, you don’t know any of the material and you're going to learn it in class anyway."
+//     }
+// ];
 
-var counter = 0;
+// var counter = 0;
 
-function renderLevelOne() {
-    document.getElementById('level-one').removeAttribute('hidden');
-    var situationOneEl = document.getElementById("level-one-question");
-    situationOneEl.innerHTML = levelOneSituations[counter].situation;
-}
+// function renderLevelOne() {
+//     document.getElementById('level-one').removeAttribute('hidden');
+//     var situationOneEl = document.getElementById("level-one-question");
+//     situationOneEl.innerHTML = levelOneSituations[counter].situation;
+// }
 
-function startGame() {
-    startBtnEl.addEventListener('click', function(event) {
-        event.preventDefault();
-        renderLevelOne();
-    })    
-}
+// function startGame() {
+//     startBtnEl.addEventListener('click', function(event) {
+//         event.preventDefault();
+//         renderLevelOne();
+//     })    
+// }
 
-startGame()
+// startGame()
 
 
 
 
-c
+// c
 
 
 
@@ -699,7 +699,7 @@ c
 
 // Start of Lvl 2
 
-var situations = [
+var lvlTwoSituations = [
     {
         situation: "After the first week you’re assigned your first challenge! It’s due a week from the assignment date.",
         cOne: "Set aside time each day to work on it, and when you have questions during the week you go to office hours to ask the instructor.",
@@ -716,11 +716,27 @@ var situations = [
 ];
 
 var sCounter = 0;
-
+// user gets to lvl two - the first situation is on the screen with the two choices underneath.
+// the user answers the question by clicking one of the choices. 
+// the next situation pops up with the next two choices.  
 function renderLvlTwo() {
     var situationEl = document.getElementById("sit");
-    situationEl.innerHTML = situations[sCounter].situation;
-    console.log(situationEl.innerHTML = situations[sCounter].situation)
+    situationEl.innerHTML = "Situation: " + "<br>" + lvlTwoSituations[sCounter].situation;
+    for (var i = 0; i < lvlTwoSituations[sCounter].situation.length; i++) {
+       var chOne = document.getElementById("chOne");
+       var chTwo = document.getElementById("chTwo");
+       chOne.textContent = lvlTwoSituations[sCounter].cOne[i];
+       chTwo.textContent = lvlTwoSituations[sCounter].cTwo[i];
+        
+    }
     
-}
+};
 renderLvlTwo();
+
+// for (var i = 0; i < lvlTwoSituations.length; i++) {
+//     sCounter = lvlTwoSituations[i].situation;
+//     var chOne = document.getElementById("chOne");
+//     var chTwo = document.getElementById("chTwo");
+    
+
+// }
