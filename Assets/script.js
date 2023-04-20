f114942418a35300150b35c3d4989cc047e15efb
jamieholpuch <125837353+jamieholpuch@users.noreply.github.com>
2023-04-20 13:38:45 -0400

    updated fetch api for results page

--- Assets/script.js
+++ Assets/script.js
@@ -60,11 +60,12 @@ function getApi(requestUrl3) {
       })
     }
       
-var universityRequestUrl = "https://universities.hipolabs.com/search?country=United+States&limit=20"
+//var universityRequestUrl = "https://universities.hipolabs.com/search?country=United+States&limit=20"
+var activityRequestUrl = "https://www.boredapi.com/api/activity?type=education"
 var universityList = document.getElementById('universityList')
 
-function getUniversityApi(universityRequestUrl) {
-  fetch(universityRequestUrl)
+function getActivityApi(activityRequestUrl) {
+  fetch(activityRequestUrl)
     .then(function (response) {
       console.log(response);
         if (response.status === 200) {
@@ -75,21 +76,21 @@ function getUniversityApi(universityRequestUrl) {
     })
     .then(function (data) {
       console.log(data);
-      var btnOne = document.getElementById("btn1");
-      var btnTwo = document.getElementById("btn2");
-      var btnThree = document.getElementById("btn3");
-      var btnFour = document.getElementById("btn4");
-      var btnFive = document.getElementById("btn5");
-      btnOne.textContent = data[0].name;
-      btnTwo.textContent = data[1].name;
-      btnThree.textContent = data[2].name;
-      btnFour.textContent = data[3].name;
-      btnFive.textContent = data[4].name;
-      document.getElementById('btn-one-href').href = data[0].web_pages[0]
-      document.getElementById('btn-two-href').href = data[1].web_pages[1]
-      document.getElementById('btn-three-href').href = data[2].web_pages[2]
-      document.getElementById('btn-four-href').href = data[3].web_pages[3]
-      document.getElementById('btn-five-href').href = data[4].web_pages[4]
+       var btnOne = document.getElementById("btn1");
+      // var btnTwo = document.getElementById("btn2");
+      // var btnThree = document.getElementById("btn3");
+      // var btnFour = document.getElementById("btn4");
+      // var btnFive = document.getElementById("btn5");
+       btnOne.textContent = data.activity;
+      // btnTwo.textContent = data[1].name;
+      // btnThree.textContent = data[2].name;
+      // btnFour.textContent = data[3].name;
+      // btnFive.textContent = data[4].name;
+       document.getElementById('btn-one-href').href = data.link;
+      // document.getElementById('btn-two-href').href = data[1].web_pages[1]
+      // document.getElementById('btn-three-href').href = data[2].web_pages[2]
+      // document.getElementById('btn-four-href').href = data[3].web_pages[3]
+      // document.getElementById('btn-five-href').href = data[4].web_pages[4]
     })
 }
 
@@ -98,7 +99,7 @@ function showResult() {
     document.getElementById('results-description').textContent = ("Not enroll in Coding Bootcamp at this time. It's a big challenge that requires a lot of time dedicated to learning and practicing. Eat, Sleep, Breathe, Code")
     universityList.removeAttribute('hidden'); 
     getApi(requestUrl3);
-    getUniversityApi(universityRequestUrl);
+    getActivityApi(activityRequestUrl);
   } else if (score <= 6) {
       document.getElementById('results-description').textContent = ("Think about it a bit more before enrolling. It sounds like you're fairly interested but not quite ready to make the necessary lifestyle changes to succeed.") 
       getApi(requestUrl2);
@@ -178,5 +179,4 @@ Share = {
   window.open(url,'','toolbar=0,status=0,width=626, height=436');
   }
   };
-
 
